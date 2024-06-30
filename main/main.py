4a7f04429003af78e0a1a719f74b2fb8b46711e0
Brendan Muldoon <Brendy@Emmas-MacBook-Air.local>
2024-06-30 19:03:23 +0100

    Add logs to main

--- main/main.py
+++ main/main.py
@@ -1,18 +1,24 @@
 from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
-
+from logging.config import dictConfig
+import logging
+from main.config.log_config import LogConfig
 from main.cache.weather_cache_singleton import get_weather_cache
 from main.controller import weather_controller
+from main.utils.weather_constants import LOGGER_NAME
+
+dictConfig(LogConfig().model_dump())
+logger = logging.getLogger(LOGGER_NAME)
 
 weather_cache = get_weather_cache()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    logger.info("Pre loading cache during application startup")
     weather_cache.load()
     yield
-    print("Application shutdown")
+    logger.info("Application shutdown")
 
 app = FastAPI(lifespan=lifespan)
 
