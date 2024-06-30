f7cb60560e678da62e453f58f908d9e52f2513b3
Brendan Muldoon <Brendy@Emmas-MacBook-Air.local>
2024-06-30 19:21:57 +0100

    Add rate limiter

--- main/controller/weather_controller.py
+++ main/controller/weather_controller.py
@@ -1,10 +1,14 @@
-from logging.config import dictConfig
 import logging
-from main.config.log_config import LogConfig
+from logging.config import dictConfig
+
 from fastapi import APIRouter, Depends, Path
 from fastapi.params import Query
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from starlette.requests import Request
 from starlette.responses import JSONResponse
 
+from main.config.log_config import LogConfig
 from main.entity.final_response import FinalResponse
 from main.service.abstract_weather_service import AbstractWeatherService
 from main.service.weather_service import WeatherService
@@ -12,12 +16,17 @@ from main.utils.weather_constants import LOGGER_NAME
 
 router = APIRouter()
 
+limiter = Limiter(key_func=get_remote_address)
+
+
 dictConfig(LogConfig().model_dump())
 logger = logging.getLogger(LOGGER_NAME)
 
 
 @router.post("/weather/{city}", response_model=FinalResponse, response_model_exclude_none=True)
-def post_weather_data(city: str = Path(..., description="The city to get weather data for"),
+@limiter.limit("5/minute")
+def post_weather_data(request: Request,
+                      city: str = Path(..., description="The city to get weather data for"),
                       service: AbstractWeatherService = Depends(WeatherService)):
     logger.info("Received POST request with path : "+city)
     response = service.create_weather_data(city)
@@ -26,7 +35,9 @@ def post_weather_data(city: str = Path(..., description="The city to get weather
 
 
 @router.get("/weather", response_model_exclude_none=True)
-def get_weather_data(date: str = Query(..., description="The date to get weather data for"),
+@limiter.limit("5/minute")
+def get_weather_data(request: Request,
+                     date: str = Query(..., description="The date to get weather data for"),
                      service: AbstractWeatherService = Depends(WeatherService)):
     logger.info("Received GET request with date : "+date)
     response = service.get_weather_data(date)
